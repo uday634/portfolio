d9fbe9372f4079aea9b7d3a223970f0081038d64
uday634 <uk8096386837@gmail.com>
2023-11-18 20:16:36 +0530

    nav bar

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import NavBar from "./components/NavBar/navbar";
+import Intro from './components/Intro/intro';
+
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar></NavBar>
+      <Intro></Intro>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Intro/intro.js
@@ -0,0 +1,11 @@
+import './intro.css'
+
+const Intro = () => {
+    return (
+        <section id='intro'>
+            
+        </section>
+    )
+}
+
+export default Intro;
